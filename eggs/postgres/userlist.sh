5f8cb250d1000763d54a81841a9d59946668cda5
Îømmi Mandela Fuentes <enum314@gmail.com>
2025-11-11 03:20:27 +0800

    Update userlist.sh to use SERVER_PORT variable for PostgreSQL connection, enhancing configurability.

--- eggs/postgres/userlist.sh
+++ eggs/postgres/userlist.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-psql -h 127.0.0.1 -At -U ${PGUSER} --dbname ${PGDBNAME} \
+psql -h 127.0.0.1 -p ${SERVER_PORT} -At -U ${PGUSER} --dbname ${PGDBNAME} \
   -c "SELECT rolname, rolpassword FROM pg_authid WHERE rolpassword LIKE 'SCRAM-SHA-256%';" \
   | awk -F '|' '{print "\"" $1 "\" \"" $2 "\""}' > /home/container/pgbouncer/userlist.txt
